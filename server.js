cf6836e0fd899f22c1c89358987a759cbea1296d
SofaNa-Stor <nagmigued@gmail.com>
2026-08-10 21:57:26 +0100

    Add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "admin.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`SofaNa store running on port ${PORT}`);
+});
